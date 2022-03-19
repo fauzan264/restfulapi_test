a2c6feb142bc0aba61d4836591095977eadb69e7
ahmad fauzan <fauzan.c4@gmail.com>
2022-03-20 04:02:34 +0700

    create order

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -3,8 +3,38 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Products;
+use App\Models\Orders;
+use Carbon\Carbon;
 
-class OrderController extends Controller
+class OrderController extends BaseController
 {
-    //
+    public function store() {
+        // cari data produk berdasarkan product_id
+        $product = Products::find(\request('product_id'));
+
+        // jika produk tidak ditemukan
+        if($product == NULL) {
+            // kembalikan nilai dengan format produk tidak ditemukan
+            return $this->out(
+                status  : "Gagal",
+                code    : 404,
+                error   : ["Produk tidak ditemukan"]
+            );
+        }
+
+        $order = new Orders();
+        $order->order_date  = Carbon::now('Asia/Jakarta');
+        $order->product_id  = $product->id;
+        $order->customer_id = request('customer_id');
+        $order->quantity    = request('quantity');
+        $order->price       = $product->price;
+
+        // jika operasi insert berhasil
+        if ($order->save() == true) {
+            return $this->out(data: $order, status: "OK", code:201);
+        } else {
+            return $this->out(status: "Gagal", error: ["Order gagal disimpan"], code: 504);
+        }
+    }
 }
