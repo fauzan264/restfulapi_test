b1a033c291099914f1205c87b93771bbc3dc5877
ahmad fauzan <fauzan.c4@gmail.com>
2022-03-20 03:21:26 +0700

    create ProductController and method find all

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Products;
+
+class ProductController extends BaseController
+{
+    public function findAll() {
+        $data = Products::paginate(20,
+                [
+                    "id", "title", "category", "price","stock", "free_shipping", "rate"
+                ]);
+
+        if (count($data) == 0) {
+            return $this->out(data:[], status:"Kosong", code:204); // 204 == content empty
+        } else {
+            return $this->out(data: $data, status: "OK");
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,3 +21,5 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/auth', [AuthController::class, "auth"]);
+
+Route::get('/product', [ProductController::class, "findAll"]);
