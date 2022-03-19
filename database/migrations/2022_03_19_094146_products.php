fc39f53a4756774d6dd67a8156bb2f65e45a4bfb
ahmad fauzan <fauzan.c4@gmail.com>
2022-03-19 17:24:34 +0700

    create migrate products, customers, and orders

--- database/migrations/2022_03_19_094146_products.php
+++ database/migrations/2022_03_19_094146_products.php
@@ -13,14 +13,15 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
-            $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+        Schema::create('products', function(Blueprint $b){
+            $b->id();
+            $b->string('title', 80);
+            $b->string('category', 50);
+            $b->double('price');
+            $b->text('descriptions');
+            $b->integer('stock');
+            $b->boolean('free_shipping');
+            $b->double('rate');
         });
     }
 
@@ -31,6 +32,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::drop('products');
     }
 };
